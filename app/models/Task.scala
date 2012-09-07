70b69f2121e9ef9b4254ca50ab7ee88f849d1f9c
Nelson Chen <crazysim@gmail.com>
2012-09-07 13:22:59 -0700

    setup scalaquery for use

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -3,36 +3,35 @@ package models
 import anorm._
 import anorm.SqlParser._
 
+import org.scalaquery.ql._
+import org.scalaquery.ql.TypeMapper._
+import org.scalaquery.ql.extended.{ExtendedTable => Table}
+
 import play.api.db._
 import play.api.Play.current
 
+import org.scalaquery.ql.extended.H2Driver.Implicit._
+
+import org.scalaquery.session._
+
 case class Task(id: Long, label: String)
 
-object Task {
-  val task = {
-    get[Long]("id") ~
-      get[String]("label") map {
-      case id ~ label => Task(id, label)
-    }
-  }
-  def all(): List[Task] = DB.withConnection { implicit c =>
-    SQL("select * from task").as(task *)
-  }
+object Task extends Table[(Long, String)]("task") {
+
+  lazy val database = Database.forDataSource(DB.getDataSource())
+
+  def id = column[Long]("id", O PrimaryKey, O AutoInc)
+
+  def label = column[String]("label")
+
+  def * = id ~ label
+
+  def all(): List[Task] = List()
 
   def create(label: String) {
-    DB.withConnection { implicit c =>
-      SQL("insert into task (label) values ({label})").on(
-        'label -> label
-      ).executeUpdate()
-    }
   }
 
   def delete(id: Long) {
-    DB.withConnection { implicit c =>
-      SQL("delete from task where id = {id}").on(
-        'id -> id
-      ).executeUpdate()
-    }
   }
 
 }
