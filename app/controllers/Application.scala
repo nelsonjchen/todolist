43fa256cf441fa43a1bf260f4e9bee15a2d5dec9
Nelson Chen <crazysim@gmail.com>
2012-09-07 12:13:26 -0700

    new tasks

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -20,14 +20,15 @@ object Application extends Controller {
     Ok(views.html.index(Task.all(), taskForm))
   }
 
-  def newTask = Action { implicit request =>
-    taskForm.bindFromRequest.fold(
-      errors => BadRequest(views.html.index(Task.all(), errors)),
-      label => {
-        Task.create(label)
-        Redirect(routes.Application.tasks)
-      }
-    )
+  def newTask = Action {
+    implicit request =>
+      taskForm.bindFromRequest.fold(
+        errors => BadRequest(views.html.index(Task.all(), errors)),
+        label => {
+          Task.create(label)
+          Redirect(routes.Application.tasks)
+        }
+      )
   }
 
   def deleteTask(id: Long) = Action {
